b444217d8502931e882d2607fa1eed7059bc4b4a
Johan Silkens <SilkensJohan@gmail.com>
2016-12-15 11:29:31 +0100

    creating models

---
+++ RetroBlocks/RetroBlocks/Model/Block.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace RetroBlocks.Model
+{
+    public enum Block
+    {
+        [Description("red")]
+        RED,
+        [Description("blue")]
+        BLUE,
+        [Description("green")]
+        GREEN,
+        [Description("lblue")]
+        LBLUE,
+        [Description("lgreen")]
+        LGREEN,
+        [Description("pink")]
+        PINK,
+        [Description("yellow")]
+        YELLOW
+    }
+}
--- RetroBlocks/RetroBlocks/Model/Piece.cs
+++ RetroBlocks/RetroBlocks/Model/Piece.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RetroBlocks.Model
 {
+
+   public enum EnumPiece
+    {
+        O,
+        I,
+        S,
+        Z,
+        L,
+        J,
+        T
+    }
+   
+   
     class Piece
     {
+        private EnumPiece _kind;
+        private MultiList blockGrid;
+
+        public Piece(EnumPiece kind)
+        {
+            _kind = kind;
+            this.blockGrid = new MultiList(4,4);
+            createPiece();
+        }
+
+
+        // Create a new piece based on it's enum type
+        private void createPiece()
+        {
+            if ((_kind = EnumPiece.I) != EnumPiece.O)
+            {
+                
+            }
+        }
     }
+
+
 }
