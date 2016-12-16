b673e2bdf86bb63720606381f780a8d951c3b4db
Johan Silkens <SilkensJohan@gmail.com>
2016-12-16 18:34:49 +0100

    Working on models

--- RetroBlocks/RetroBlocks/Model/Piece.cs
+++ RetroBlocks/RetroBlocks/Model/Piece.cs
@@ -90,83 +90,91 @@ namespace RetroBlocks.Model
 
         private void CreateLPiece()
         {
-            _blockGrid.Add(1, 1, Block.YELLOW);
-            _blockGrid.Add(1, 2, Block.YELLOW);
-            _blockGrid.Add(1, 3, Block.YELLOW);
-            _blockGrid.Add(2, 1, Block.YELLOW);
+            _blockGrid.Add(1, 1, Block.LGREEN);
+            _blockGrid.Add(1, 2, Block.LGREEN);
+            _blockGrid.Add(1, 3, Block.LGREEN);
+            _blockGrid.Add(2, 1, Block.LGREEN);
 
         }
 
         /*
-       -  -  -  -
-       -  X  X  X
-       -  -  -  X
-       -  -  -  -
+       0  1  2  3
+  0    -  -  -  -
+  1    -  X  X  X
+  2    -  -  -  X
+  3    -  -  -  -
       */
 
         private void CreateJPiece()
         {
-            _blockGrid.Add(1, 1, Block.YELLOW);
-            _blockGrid.Add(1, 2, Block.YELLOW);
-            _blockGrid.Add(1, 2, Block.YELLOW);
-            _blockGrid.Add(2, 2, Block.YELLOW);
+            _blockGrid.Add(1, 1, Block.GREEN);
+            _blockGrid.Add(1, 2, Block.GREEN);
+            _blockGrid.Add(1, 3, Block.GREEN);
+            _blockGrid.Add(2, 3, Block.GREEN);
         }
 
         /*
-        -  -  -  -
-        -  -  X  X
-        -  X  X  -
-        -  -  -  -
+        0  1  2  3
+  0     -  -  -  -
+  1     -  -  X  X
+  2     -  X  X  -
+  3     -  -  -  -
        */
 
         private void CreateSPiece()
         {
-            _blockGrid.Add(1, 1, Block.YELLOW);
-            _blockGrid.Add(1, 2, Block.YELLOW);
-            _blockGrid.Add(1, 2, Block.YELLOW);
-            _blockGrid.Add(2, 2, Block.YELLOW);
+            _blockGrid.Add(1, 2, Block.PINK);
+            _blockGrid.Add(1, 3, Block.PINK);
+            _blockGrid.Add(2, 1, Block.PINK);
+            _blockGrid.Add(2, 2, Block.PINK);
         }
 
 
         /*
-         -  -  -  -
-         -  X  X  -
-         -  -  X  X
-         -  -  -  -
+         0  1  2  3
+      0  -  -  -  -
+      1  -  X  X  -
+      2  -  -  X  X
+      3  -  -  -  -
         */
         private void CreateZPiece()
         {
-            throw new NotImplementedException();
+            _blockGrid.Add(1, 1, Block.RED);
+            _blockGrid.Add(1, 2, Block.RED);
+            _blockGrid.Add(2, 2, Block.RED);
+            _blockGrid.Add(2, 3, Block.RED);
         }
 
         /*
-         -  -  -  -
-         -  X  X  -
-         -  X  X  -
-         -  -  -  -
+         0  1  2  3
+  0      -  -  -  -
+  1      -  X  X  -
+  2      -  X  X  -
+  3      -  -  -  -
         */
         private void CreateOPiece()
         {
             _blockGrid.Add(1,1,Block.YELLOW);
             _blockGrid.Add(1,2, Block.YELLOW);
-            _blockGrid.Add(1,2, Block.YELLOW);
+            _blockGrid.Add(2,1, Block.YELLOW);
             _blockGrid.Add(2,2, Block.YELLOW);
         }
 
 
         /*
-        -  -  -  -
-        X  X  X  X
-        -  -  -  -
-        -  -  -  -
+        0  1  2  3
+    0   -  -  -  -
+    1   X  X  X  X
+    2   -  -  -  -
+    3   -  -  -  -
         */
         private void CreateIPiece()
         {
            
-                _blockGrid.Add(1, 0,Block.BLUE);
-                _blockGrid.Add(1, 1, Block.BLUE);
-                _blockGrid.Add(1, 2, Block.BLUE);
-                _blockGrid.Add(1, 3, Block.BLUE);
+                _blockGrid.Add(1, 0,Block.LBLUE);
+                _blockGrid.Add(1, 1, Block.LBLUE);
+                _blockGrid.Add(1, 2, Block.LBLUE);
+                _blockGrid.Add(1, 3, Block.LBLUE);
         }
 
         public BlockArray BlockGrid
@@ -176,12 +184,16 @@ namespace RetroBlocks.Model
 
         public void RotateLeft()
         {
+            // transpose matrix
+            // reverse each column
+
             
         }
 
         public void RotateRight()
         {
-            
+            // transpose matrix
+            // reverse each row
         }
     }
 
