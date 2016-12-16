ee79d3dcac0b0014f74ddefebbd5d1d34e1bcbc4
Johan Silkens <SilkensJohan@gmail.com>
2016-12-16 20:41:03 +0100

    Working on models

--- RetroBlocks/RetroBlocks/Model/BlockArray.cs
+++ RetroBlocks/RetroBlocks/Model/BlockArray.cs
@@ -21,20 +21,46 @@ namespace RetroBlocks.Model
 
         public BlockArray(int height,int width)
         {
-            _blockArray = new Block[height,width];
+            _blockArray = new Block[width,height];
         }
 
 
         public Block Get(int x, int y)
         {
-            return _blockArray[y, x];
+            return _blockArray[x, y];
 
     
         }
 
         public void Add(int x, int y, Block b)
         {
-            _blockArray[y, x] = b;
+            _blockArray[x, y] = b;
+        }
+
+
+        public void Transpose()
+        {
+            Block[,] newBlockArray = new Block[_blockArray.GetLength(0),_blockArray.GetLength(1)];
+            for (int i = 0; i < _blockArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < _blockArray.GetLength(1); j++)
+                {
+                    newBlockArray[i, j] = _blockArray[j, i];
+                }
+            }
+
+            _blockArray = newBlockArray;
+
+        }
+
+        public void ReverseAllRows()
+        {
+            
+        }
+
+        public void ReverseAllColums()
+        {
+            
         }
     }
 }
--- RetroBlocks/RetroBlocks/Model/Piece.cs
+++ RetroBlocks/RetroBlocks/Model/Piece.cs
@@ -74,8 +74,8 @@ namespace RetroBlocks.Model
         private void CreateTPiece()
         {
             _blockGrid.Add(1, 1, Block.BLUE);
-            _blockGrid.Add(1, 2, Block.BLUE);
-            _blockGrid.Add(1, 3, Block.BLUE);
+            _blockGrid.Add(2, 1, Block.BLUE);
+            _blockGrid.Add(3, 1, Block.BLUE);
             _blockGrid.Add(2, 2, Block.BLUE);
 
         }
@@ -108,9 +108,9 @@ namespace RetroBlocks.Model
         private void CreateJPiece()
         {
             _blockGrid.Add(1, 1, Block.GREEN);
-            _blockGrid.Add(1, 2, Block.GREEN);
-            _blockGrid.Add(1, 3, Block.GREEN);
-            _blockGrid.Add(2, 3, Block.GREEN);
+            _blockGrid.Add(2, 1, Block.GREEN);
+            _blockGrid.Add(3, 1, Block.GREEN);
+            _blockGrid.Add(3, 2, Block.GREEN);
         }
 
         /*
@@ -123,9 +123,9 @@ namespace RetroBlocks.Model
 
         private void CreateSPiece()
         {
-            _blockGrid.Add(1, 2, Block.PINK);
-            _blockGrid.Add(1, 3, Block.PINK);
             _blockGrid.Add(2, 1, Block.PINK);
+            _blockGrid.Add(3, 1, Block.PINK);
+            _blockGrid.Add(1, 2, Block.PINK);
             _blockGrid.Add(2, 2, Block.PINK);
         }
 
@@ -140,9 +140,9 @@ namespace RetroBlocks.Model
         private void CreateZPiece()
         {
             _blockGrid.Add(1, 1, Block.RED);
-            _blockGrid.Add(1, 2, Block.RED);
+            _blockGrid.Add(2, 1, Block.RED);
             _blockGrid.Add(2, 2, Block.RED);
-            _blockGrid.Add(2, 3, Block.RED);
+            _blockGrid.Add(3, 2, Block.RED);
         }
 
         /*
@@ -155,8 +155,8 @@ namespace RetroBlocks.Model
         private void CreateOPiece()
         {
             _blockGrid.Add(1,1,Block.YELLOW);
-            _blockGrid.Add(1,2, Block.YELLOW);
             _blockGrid.Add(2,1, Block.YELLOW);
+            _blockGrid.Add(1,2, Block.YELLOW);
             _blockGrid.Add(2,2, Block.YELLOW);
         }
 
@@ -171,10 +171,10 @@ namespace RetroBlocks.Model
         private void CreateIPiece()
         {
            
-                _blockGrid.Add(1, 0,Block.LBLUE);
+                _blockGrid.Add(0, 1,Block.LBLUE);
                 _blockGrid.Add(1, 1, Block.LBLUE);
-                _blockGrid.Add(1, 2, Block.LBLUE);
-                _blockGrid.Add(1, 3, Block.LBLUE);
+                _blockGrid.Add(2, 1, Block.LBLUE);
+                _blockGrid.Add(3, 1, Block.LBLUE);
         }
 
         public BlockArray BlockGrid
@@ -185,7 +185,9 @@ namespace RetroBlocks.Model
         public void RotateLeft()
         {
             // transpose matrix
+            _blockGrid.Transpose();
             // reverse each column
+            _blockGrid.ReverseAllColums();
 
             
         }
@@ -193,7 +195,9 @@ namespace RetroBlocks.Model
         public void RotateRight()
         {
             // transpose matrix
+            _blockGrid.Transpose();
             // reverse each row
+            _blockGrid.ReverseAllRows();
         }
     }
 
